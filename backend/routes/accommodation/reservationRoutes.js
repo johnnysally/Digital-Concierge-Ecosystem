ee228d225d4b016c804250c321a2b858659373c2
Davix HDM <davismcintyre5@gmail.com>
2026-08-04 10:01:24 +0300

    Fixed Route

--- backend/routes/accommodation/reservationRoutes.js
+++ backend/routes/accommodation/reservationRoutes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
-const { getReservations, getReservation, updateReservationStatus, updateReservationPaymentStatus, createReservation, deleteReservation } = require('../../controllers/accommodation/reservationController');
+const { createReservation, getReservations, getReservation, updateReservationStatus, deleteReservation } = require('../../controllers/accommodation/reservationController');
 const accommodationAuth = require('../../middleware/accommodation/accommodationAuth');
 
 router.use(accommodationAuth);
+router.post('/', createReservation);
 router.get('/', getReservations);
 router.get('/:id', getReservation);
 router.put('/:id/status', updateReservationStatus);
-router.put('/:id/payment-status', updateReservationPaymentStatus);
-router.post('/', createReservation);
 router.delete('/:id', deleteReservation);
 
 module.exports = router;
