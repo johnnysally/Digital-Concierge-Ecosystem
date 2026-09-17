6dc5df8cee63b706f8d52ddaa7f595aec432732a
John Mwangi <sallywanjiru860@gmail.com>
2026-09-17 18:04:55 +0300

    bug fix

--- frontend/src/api/accommodation/authApi.ts
+++ frontend/src/api/accommodation/authApi.ts
@@ -14,6 +14,7 @@ export interface RegisterPayload {
     businessName: string;
     businessType?: string;
     towns?: string[];
+    town?: string[];
 }
 
 export const login = async (payload: LoginPayload) => {
